c99b24f8692bf393d059da4726fe5135ceb0d7e3
treemcgee42 <varun.malladi@gmail.com>
2021-06-12 14:35:15 -0500

    Basic input loop for command

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,17 @@
 use std::io;
+use std::io::Write;
+use std::env;
 
 fn main() {
-    println!("Hello, world!");
+    loop {
+        let mut current_dir = env::current_dir().unwrap();
+        print!("{}> ", current_dir.display());
+        io::stdout().flush().unwrap();
+
+        let mut command = String::new();
+        io::stdin().read_line(&mut command)
+            .expect("Failed to read command.");
+
+        println!("{}",command.trim());
+    }
 }
