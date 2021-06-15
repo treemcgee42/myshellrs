055df4c6c8d1c1fe2a324b6fb47b8b900cb9fcb2
treemcgee42 <varun.malladi@gmail.com>
2021-06-15 18:02:33 -0500

    added in ls-util binary and better argument parsing using match

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,33 @@
-mod ls;
 
 use std::io;
 use std::io::Write;
 use std::env;
+use std::process::Command;
 
 fn main() {
+    let mut working_dir = env::current_dir().unwrap();
     loop {
-        let mut current_dir = env::current_dir().unwrap();
-        print!("{}> ", current_dir.display());
+        print!("{}> ", working_dir.display());
         io::stdout().flush().unwrap();
 
         let mut command = String::new();
         io::stdin().read_line(&mut command)
             .expect("Failed to read command.");
+        command = command.trim().to_string();
 
-        if command=="ls\n" {
-            ls::ls(&current_dir).unwrap();
+        let mut args = command.split(' ');
+
+        match args.next() {
+            None => (),
+            Some("") => (),
+            Some("ls") => {
+                let arg_string = args.fold(String::new(), |acc,x| acc+x);
+                Command::new("./ls-util").arg(arg_string).status()
+                    .expect("`ls` failed to run");
+            }
+            Some(_) => {
+                println!("Unknown command");
+            }
         }
     }
 }
