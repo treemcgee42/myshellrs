fe9d5f9ab742a1ea9ab776c924ad4c5409512949
treemcgee42 <varun.malladi@gmail.com>
2021-06-15 17:16:45 -0500

    allowed ls functionality for other directories, not just the current one

--- src/ls.rs
+++ src/ls.rs
@@ -6,7 +6,7 @@ use std::time::SystemTime;
 use stdout_tables::tables::Table;
 use stdout_tables::themes::Theme;
 
-pub fn ls(dir: &Path) -> Result<(), Box<Error>> {
+pub fn list_contents(dir: &Path) -> Result<(), Box<Error>> {
     if dir.is_dir() {
         let headers: Vec<(Option<usize>,String)> = vec![
             (Some(3),String::from("#")), (Some(15),String::from("name")), 
@@ -25,10 +25,12 @@ pub fn ls(dir: &Path) -> Result<(), Box<Error>> {
                 .into_string()
                 .or_else(|f| Err(format!("Invalid entry: {:?}", f)))?;
             let file_type: String;
-            let file_size: u64 = metadata.len();
+            let file_size: String = parse_bytes(&metadata.len());
             let last_modified: String = 
                 match metadata.modified() {
-                    Ok(n) => format!("{}",SystemTime::now().duration_since(n).unwrap().as_secs()),
+                    Ok(n) => parse_secs(
+                        &SystemTime::now().duration_since(n).unwrap().as_secs()
+                    ),
                     _ => String::from(""),
                 };
             
@@ -43,7 +45,7 @@ pub fn ls(dir: &Path) -> Result<(), Box<Error>> {
                     format!("{}",i), 
                     name, 
                     file_type,
-                    format!("{}",file_size),
+                    file_size,
                     last_modified
                 ]
             );
@@ -54,4 +56,58 @@ pub fn ls(dir: &Path) -> Result<(), Box<Error>> {
         t.draw(Theme::heavy());
     }
     Ok(())
+}
+
+fn parse_secs(secs: &u64) -> String {
+    // values in seconds
+    let minute:u64 = 60;
+    let hour:u64 = minute*60;
+    let day = hour*24;
+    let week = day*27;
+    let year = week*52;
+
+    // by minutes (0-59 mins)
+    if secs<&hour {
+        format!("{} minutes ago", secs/&minute)
+    }
+    // by hours (1-23 hrs)
+    else if secs<&day {
+        format!("{} hours ago", secs/&hour)
+    }
+    // by days (1-7 days)
+    else if secs<&week {
+        format!("{} days ago", secs/&day)
+    }
+    // by weeks (1-51 weeks)
+    else if secs<&year {
+        format!("{} weeks ago", secs/&week)
+    }
+    // by years (1+ yrs)
+    else {
+        format!("{} years ago", secs/&year)
+    }
+}
+
+fn parse_bytes(bs: &u64) -> String {
+    // in terms of bytes
+    let kb: u64 = 1000;
+    let mb: u64 = 1000*kb;
+    let gb: u64 = 1000*mb;
+
+    // by bytes (0-999 b)
+    if bs<&kb {
+        format!("{} b", bs)
+    }
+    // by kilobytes (1-999 kb)
+    else if bs<&mb {
+        format!("{} kb", bs/&kb)
+    }
+    // by megabytes (1-999 mb)
+    else if bs<&gb {
+        format!("{} mb", bs/&mb)
+    }
+    // by gigabytes (1+ gb)
+    else {
+        format!("{} gb", bs/&gb)
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,18 @@ mod ls;
 use std::env;
 
 fn main() {
-    let current_dir = env::current_dir().unwrap();
-    
-    ls::ls(&current_dir);
+    let mut working_dir = env::current_dir().unwrap();
+
+    let args: Vec<String> = env::args().collect();
+
+    if args.len()>1 {
+        working_dir.push(&args[1]);
+        if !working_dir.is_dir() {
+            println!("{} is not a directory", &args[1]);
+        } else {
+            ls::list_contents(&working_dir);
+        }
+    } else {
+        ls::list_contents(&working_dir);
+    }
 }
