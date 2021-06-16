b6a5bc7e4810677c64126c522dac734613703fce
treemcgee42 <varun.malladi@gmail.com>
2021-06-15 21:48:44 -0500

    added cd functionality compatible with ls

--- src/main.rs
+++ src/main.rs
@@ -21,9 +21,26 @@ fn main() {
             None => (),
             Some("") => (),
             Some("ls") => {
-                let arg_string = args.fold(String::new(), |acc,x| acc+x);
-                Command::new("./ls-util").arg(arg_string).status()
+                // let arg_string = args.fold(String::new(), |acc,x| acc+x) 
+                //     + " --default-dir "+&working_dir.clone().into_os_string().into_string()
+                //     .expect("non-unicode os-string");
+                Command::new("./ls-util")
+                    .args(args).arg("--default-dir").arg(working_dir.clone().into_os_string().into_string().unwrap())
+                    .status()
                     .expect("`ls` failed to run");
+            }
+            Some("cd") => {
+                match args.next() {
+                    None => (),
+                    Some("..") => {
+                        working_dir.pop();
+                        ()
+                    }
+                    Some(s) => working_dir.push(s),
+                }
+            }
+            Some("clear") => {
+                print!("{esc}[2J{esc}[1;1H", esc = 27 as char);
             }
             Some(_) => {
                 println!("Unknown command");
